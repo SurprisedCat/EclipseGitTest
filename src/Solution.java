021ee4d9c1572b88c6508e05361a8f0b4b34255f
lelouch <840025951@163.com>
2016-10-14 15:10:02 +0800

    A First Test

---
+++ src/Solution.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Solution {
+
+    /*
+     * @param numbers : An array of Integer
+     * @param target : target = numbers[index1] + numbers[index2]
+     * @return : [index1 + 1, index2 + 1] (index1 < index2)
+         numbers=[2, 7, 11, 15],  target=9
+         return [1, 2]
+     */
+    public int[] twoSum(int[] numbers, int target) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (map.get(numbers[i]) != null) {
+                int[] result = {map.get(numbers[i]) + 1, i + 1};
+                return result;
+            }
+            map.put(target - numbers[i], i);
+        }
+        
+        int[] result = {};
+        return result;
+ }
+    
+    public static void main(String[] args)
+    {
+    	Solution mysolution = new Solution();
+    	int[] numbers = {2,7,11,15};
+    	int target = 9;
+    	int[] result = mysolution.twoSum(numbers, target);
+    	for(int i =0;i<result.length;i++)
+    		System.out.println(result[i]);
+    }
+}
